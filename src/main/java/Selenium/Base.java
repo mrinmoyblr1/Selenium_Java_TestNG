d5157d277c114a18ab71bc89f42de11ec49388eb
mrinmoyblr2 <mrinmoy.blr2@gmail.com>
2025-12-03 15:49:33 -0500

    BISWAS

--- src/main/java/Selenium/Base.java
+++ src/main/java/Selenium/Base.java
@@ -11,9 +11,14 @@ import java.util.List;
 public class Base {
     public static void main(String[] args) throws InterruptedException {
 
-        String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot"};
+        //String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot","Beans"};
+        String[] itemsNeeded = {"Beans"};
         List itemNeededList = Arrays.asList(itemsNeeded);
 
+
+        for (int i = 0; i < itemNeededList.size(); i++) {
+            System.out.println(itemNeededList.get(i));
+        }
 
         System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
         WebDriver driver = new ChromeDriver();
@@ -21,13 +26,23 @@ public class Base {
 
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
-        for (int i = 0; i < products.size(); i++) {
+        int flag = 0;
+        System.out.println(itemNeededList.size());
+        System.out.println(products.size());
 
+
+        for (int i = 0; i < products.size(); i++) {
             String[] name = products.get(i).getText().split("-");
             String formattedName = name[0].trim();
+            System.out.println("=====" + i);
+
 
             if (itemNeededList.contains(formattedName)) {
                 driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
+                flag = flag + 1;
+                if (flag == itemNeededList.size() - 1) {
+                    break;
+                }
             }
         }
 
@@ -41,10 +56,12 @@ public class Base {
 //                }
 //            }
 //        }
+
+
         // XPath to click on "ADD TO CART"
         //*[contains(text(), 'Cucumber')]/following-sibling::div[2]/button
 
-        Thread.sleep(9000);
+        Thread.sleep(2000);
         driver.quit();
     }
 }
