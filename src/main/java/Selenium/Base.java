5fbd33d14125e83f0a384a22027c9ea51efa3751
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-12-04 11:12:57 -0500

    BISWAS

--- src/main/java/Selenium/Base.java
+++ src/main/java/Selenium/Base.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,22 +12,21 @@ import java.util.List;
 public class Base {
     public static void main(String[] args) throws InterruptedException {
 
-        //String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot","Beans"};
-        String[] itemsNeeded = {"Beans"};
+        int j = 0;
+        String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot", "Beans"};
+//        String[] itemsNeeded = {"Beans"};
         List itemNeededList = Arrays.asList(itemsNeeded);
-
-
         for (int i = 0; i < itemNeededList.size(); i++) {
             System.out.println(itemNeededList.get(i));
         }
 
-        System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
-        WebDriver driver = new ChromeDriver();
+
+//        System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-
-        int flag = 0;
         System.out.println(itemNeededList.size());
         System.out.println(products.size());
 
@@ -34,23 +34,24 @@ public class Base {
         for (int i = 0; i < products.size(); i++) {
             String[] name = products.get(i).getText().split("-");
             String formattedName = name[0].trim();
-            System.out.println("=====" + i);
+            System.out.println(formattedName);
 
 
             if (itemNeededList.contains(formattedName)) {
-                driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
-                flag = flag + 1;
-                if (flag == itemNeededList.size() - 1) {
+                j++;
+//                driver.findElement(By.xpath("//*[contains(text(),\"" + formattedName + "\")]/following-sibling::div[2]/button")).click();
+                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+                if (j == itemNeededList.size()) {
                     break;
                 }
             }
         }
 
 
-//        for (WebElement product : allProducts) {//
+//        for (WebElement product : products) {//
 //            for (int i = 0; i < itemsNeeded.length; i++) {//
 //                if (product.getText().contains(itemsNeeded[i])) {
-//                    driver.findElement(By.xpath("//button[text()=\"ADD TO CART\"]")).click();
+//                    driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
 //                    System.out.println(product.getText());
 //                    break;
 //                }
