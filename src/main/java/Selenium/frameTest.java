54b3592bd15a1fbf6fe06b860a808ac700a92728
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-12-12 08:55:34 -0500

    BISWAS

--- src/main/java/Selenium/frameTest.java
+++ src/main/java/Selenium/frameTest.java
@@ -1,32 +1,33 @@
 package Selenium;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
 import java.util.concurrent.TimeUnit;
-
 public class frameTest {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
         WebDriver driver = new ChromeDriver();
 //        WebDriver driver = new FirefoxDriver();
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://jqueryui.com/droppable/");
-        
-
-
-
-
-
-
-
+        driver.switchTo().frame(driver.findElement(By.cssSelector(".demo-frame")));
+        // We can switch to Frame using index also
+//        driver.switchTo().frame(0);
+        driver.findElement(By.cssSelector("#draggable")).click();
+        System.out.println(driver.findElement(By.cssSelector("#draggable")).getText());
+        Actions actions = new Actions(driver);
+        WebElement source = driver.findElement(By.cssSelector("#draggable"));
+        WebElement target = driver.findElement(By.cssSelector("#droppable"));
+        actions.dragAndDrop(source, target).build().perform();
+// To come back from iframe we need to use defaultContent()
+        driver.switchTo().defaultContent();
+        System.out.println(driver.findElement(By.cssSelector(".entry-title")).getText());
 //
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         driver.quit();
     }
 }
