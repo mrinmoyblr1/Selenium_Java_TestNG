383e4dd11a8f55459a0f304df844f78c754f0715
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-12-12 08:32:23 -0500

    BISWAS

---
+++ src/main/java/Selenium/frameTest.java
@@ -0,0 +1,32 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class frameTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
+        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://jqueryui.com/droppable/");
+        
+
+
+
+
+
+
+
+//
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
