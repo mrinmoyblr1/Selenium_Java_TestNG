9789f6fea5bdc164f3618981c4d5eb05415182da
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-12-14 14:01:10 -0500

    BISWAS

---
+++ src/main/java/Selenium/CalendarTest.java
@@ -0,0 +1,46 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class CalendarTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+
+        String monthNumber = "6";
+        String date = "15";
+        String year = "2027";
+
+        driver.findElement(By.cssSelector(".react-date-picker__inputGroup__input.react-date-picker__inputGroup__year")).click();
+        driver.findElement(By.cssSelector(".react-calendar__navigation__label__labelText.react-calendar__navigation__label__labelText--from")).click();
+
+        driver.findElement(By.cssSelector(".react-calendar__navigation__label__labelText.react-calendar__navigation__label__labelText--from")).click();
+
+        WebElement yearClick = driver.findElement(By.cssSelector(".react-calendar__decade-view"));
+        System.out.println(yearClick.findElements(By.cssSelector("button")).size());
+        Thread.sleep(2000);
+
+        for (int i = 0; i < yearClick.findElements(By.cssSelector("button")).size(); i++) {
+            System.out.println("=========");
+
+            System.out.println(yearClick.findElements(By.cssSelector("button")).get(i).getText());
+            Thread.sleep(2000);
+            if (yearClick.findElements(By.cssSelector("button")).get(i).getText().equals(year)) {
+                yearClick.findElement(By.cssSelector("button")).click();
+
+            }
+        }
+
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
