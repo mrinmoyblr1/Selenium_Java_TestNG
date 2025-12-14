8965787f1976f79fb2c1e4fd761e88408f954ba2
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-12-13 23:27:24 -0500

    BISWAS

--- src/main/java/Selenium/Scope.java
+++ src/main/java/Selenium/Scope.java
@@ -2,6 +2,7 @@ package Selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -15,6 +16,27 @@ public class Scope {
 
         System.out.println(driver.findElements(By.cssSelector("a")).size());
         System.out.println(driver.findElements(By.tagName("a")).size());
+
+        // Creating subset of a driver specific ti footer section
+        // Limiting WebDriver scope
+        WebElement footerDriver = driver.findElement(By.cssSelector(".gf-t"));
+        System.out.println(footerDriver.findElements(By.tagName("a")).size());
+
+        WebElement columnDriver = footerDriver.findElement(By.cssSelector("ul"));
+        System.out.println(columnDriver.findElements(By.cssSelector("a")).size());
+        Thread.sleep(6000);
+
+
+        String parentWindow = driver.getWindowHandle();
+
+
+        for (int i = 0; i < columnDriver.findElements(By.cssSelector("a")).size(); i++) {
+//            System.out.println(columnDriver.findElements(By.cssSelector("a")).get(i).getText());
+            columnDriver.findElements(By.cssSelector("a")).get(i).click();
+
+            driver.switchTo().window(parentWindow);
+
+        }
 
 
         Thread.sleep(2000);
