b10579ffa75140641c5d299e7356fe9e131142e9
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-12-14 06:06:01 -0500

    BISWAS

--- src/main/java/Selenium/Scope.java
+++ src/main/java/Selenium/Scope.java
@@ -9,30 +9,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class Scope {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
         WebDriver driver = new ChromeDriver();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-
         System.out.println(driver.findElements(By.cssSelector("a")).size());
         System.out.println(driver.findElements(By.tagName("a")).size());
-
         // Creating subset of a driver specific ti footer section
         // Limiting WebDriver scope
         WebElement footerDriver = driver.findElement(By.cssSelector(".gf-t"));
         System.out.println(footerDriver.findElements(By.tagName("a")).size());
-
         WebElement columnDriver = footerDriver.findElement(By.cssSelector("ul"));
         System.out.println(columnDriver.findElements(By.cssSelector("a")).size());
         Thread.sleep(6000);
-
         String osName = System.getProperty("os.name").toLowerCase();
-
         for (int i = 0; i < columnDriver.findElements(By.cssSelector("a")).size(); i++) {
             // Use Command for Mac, Control for everything else (Windows, Linux, etc.)
             if (osName.contains("mac")) {
@@ -45,8 +38,7 @@ public class Scope {
                 columnDriver.findElements(By.cssSelector("a")).get(i).sendKeys(clickOnLinkTab);
             }
         }
-
-
+        
         Set<String> abc = driver.getWindowHandles();
         Iterator<String> it = abc.iterator();
         while (it.hasNext()) {
@@ -54,11 +46,8 @@ public class Scope {
             driver.switchTo().window(key);
             System.out.println("Title: " + driver.getTitle());
         }
-
 //            // Like this we can click on Back Button in browser
 //            driver.navigate().back();
-
-
         Thread.sleep(2000);
         driver.quit();
     }
