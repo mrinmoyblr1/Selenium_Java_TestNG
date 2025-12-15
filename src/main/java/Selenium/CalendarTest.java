e8f6e941f7a074e6c6e937fbbb6dbed6b9298893
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-12-15 10:04:50 -0500

    SSS1

--- src/main/java/Selenium/CalendarTest.java
+++ src/main/java/Selenium/CalendarTest.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.Set;
 
 public class CalendarTest {
     public static void main(String[] args) throws InterruptedException {
@@ -16,46 +15,24 @@ public class CalendarTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
-
         String monthNumber = "6";
         String date = "15";
         String year = "2027";
-
+        String[] expectedList = {monthNumber, date, year};
         driver.findElement(By.cssSelector(".react-date-picker__inputGroup__input.react-date-picker__inputGroup__year")).click();
         driver.findElement(By.cssSelector(".react-calendar__navigation__label__labelText.react-calendar__navigation__label__labelText--from")).click();
         driver.findElement(By.cssSelector(".react-calendar__navigation__label__labelText.react-calendar__navigation__label__labelText--from")).click();
-
         driver.findElement(By.xpath("//button[text()='" + year + "']")).click();
         Thread.sleep(6000);
 //        driver.findElements(By.xpath(".react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber) - 1).click();
         driver.findElements(By.cssSelector(".react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber) - 1).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//abbr[text()='" + date + "']")).click();
-
-
-        System.out.println(driver.findElement(By.cssSelector(".react-date-picker__wrapper")).getText());
-
         List<WebElement> actualList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
-
-
-        for (WebElement ss : actualList) {
-
+        for (int i = 0; i < actualList.size(); i++) {
+            System.out.println(actualList.get(i).getAttribute("value"));
+            Assert.assertEquals(actualList.get(i).getAttribute("value"), expectedList[i]);
         }
-
-
-//        WebElement yearClick = driver.findElement(By.cssSelector(".react-calendar__decade-view"));
-//        System.out.println(yearClick.findElements(By.cssSelector("button")).size());
-//        Thread.sleep(2000);
-//
-//        for (int i = 0; i < yearClick.findElements(By.cssSelector("button")).size(); i++) {
-//            if (yearClick.findElements(By.cssSelector("button")).get(i).getText().equals(year)) {
-//                yearClick.findElements(By.cssSelector("button")).get(i).click();
-//                Thread.sleep(6000);
-//                break;
-//            }
-//        }
-
-
         Thread.sleep(3000);
         driver.quit();
     }
