5b6c2f0b942a9f974547e0fece4a35bd6bcd043c
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-04 05:20:57 -0500

    BISWAS

---
+++ src/main/java/TestNGTutorial/Day1.java
@@ -0,0 +1,26 @@
+package TestNGTutorial;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Day1 {
+    @Test
+    public void Demo1() {
+
+        System.out.println("Hello World from Demo1");
+//        System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://rahulshettyacademy.com/");
+//        System.out.println(driver.getTitle());
+//        driver.quit();
+    }
+
+
+    @Test
+    public void Demo2() {
+        System.out.println("Hello World from Demo2");
+    }
+
+
+}
