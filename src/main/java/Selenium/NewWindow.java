7d91fd304c5ae9a5675580fbf890156ddee6f11f
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-04 04:56:51 -0500

    BISWAS

--- src/main/java/Selenium/NewWindow.java
+++ src/main/java/Selenium/NewWindow.java
@@ -1,16 +1,16 @@
 package Selenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
 public class NewWindow {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/angularpractice/");
@@ -21,11 +21,22 @@ public class NewWindow {
         String childWindowId = it.next();
         driver.switchTo().window(childWindowId);
         driver.get("https://courses.rahulshettyacademy.com/l/products?sortKey=recommended&sortDirection=asc&page=1");
+        Thread.sleep(3000);
         String courseName = driver.findElements(By.xpath("//h2[@data-sentry-element='CardTitle']")).get(0).getText();
         System.out.println(courseName);
         driver.switchTo().window(parentWindowId);
         WebElement name = driver.findElement(By.cssSelector("[name='name']"));
         name.sendKeys(courseName);
+
+        // Take screenshot only for the specific WebElement and save it to a file
+        File file = name.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("./screenshots/logo.png"));
+
+        // Take screenshot of the entire page and save it to a file
+        File file1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file1, new File("./screenshots/logo1.png"));
+
+
         Thread.sleep(2000);
         driver.quit();
     }
