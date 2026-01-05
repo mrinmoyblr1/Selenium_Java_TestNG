b2b9188d4e812f5f8e8183c359b8f6e70b4fc18f
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-05 18:18:09 -0500

    BISWAS

--- src/main/java/TestNGTutorial/Day2.java
+++ src/main/java/TestNGTutorial/Day2.java
@@ -1,19 +1,35 @@
 package TestNGTutorial;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Day2 {
     @Test
-    public void Demo1() {
-
-        System.out.println("Hello World from Demo1");
+    public void ploan() {
+        System.out.println("good");
     }
 
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("I will execute before all tests in Day2 class");
+        // We can write code here to set up preconditions for the tests
+        // such as initializing variables, opening database connections, etc.
+        // This method will run once before any test methods in this class are executed
+        // similar to @BeforeClass but at the test level
+        // useful for setting up configurations needed for the tests
+        // in this class
+        // Note: If there are multiple classes in the same test suite,
+        // this method will run only once before all of them
+        // ensuring a clean setup for the tests
+        // This is particularly useful for integration tests
+        // where you might need to set up a test environment
+        // before running the tests
+        // It helps in maintaining test isolation and consistency
+        // across different test runs
+        // making the tests more reliable and easier to debug
+        // overall improving the quality of the test suite
+        // and the software being tested
 
-    @Test
-    public void Demo2() {
-        System.out.println("Hello World from Demo2");
+
     }
-
-
 }
