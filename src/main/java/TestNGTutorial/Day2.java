d7d374933d896c23aa319c70b625cb3cf40a1848
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-06 00:24:53 -0500

    BISWAS

--- src/main/java/TestNGTutorial/Day2.java
+++ src/main/java/TestNGTutorial/Day2.java
@@ -8,19 +8,20 @@ public class Day2 {
         System.out.println("good");
     }
 
+
     @BeforeSuite
     public void beforeSuite() {
-        System.out.println("I am no 1. I will execute before everything in the suite");
+        System.out.println("beforeSuite.....I am no 1. I will execute before everything in the suite");
     }
 
     @AfterSuite
     public void afterSuite() {
-        System.out.println("I am no 1. I will execute after everything in the suite");
+        System.out.println("afterSuite....I am no 1. I will execute after everything in the suite");
     }
 
     @BeforeTest
     public void beforeTest() {
-        System.out.println("I will execute before all tests within .....<test name=\"Personal Loan\">.....");
+        System.out.println("beforeTest....I will execute before all tests within .....<test name=\"Personal Loan\">.....");
         // We can write code here to set up preconditions for the tests
         // such as initializing variables, opening database connections, etc.
         // This method will run once before any test methods in this class are executed
@@ -33,11 +34,30 @@ public class Day2 {
 
     @AfterTest
     public void afterTest() {
-        System.out.println("I will execute after all tests within .....<test name=\"Personal Loan\">.....");
+        System.out.println("afterTest....I will execute after all tests within .....<test name=\"Personal Loan\">.....");
     }
 
     @AfterMethod
     public void afterMethod() {
-        System.out.println("I will execute after every method in Day2 class");
+        System.out.println("afterMethod.....I will execute after every method in Day2 class");
     }
+
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("beforeMethod...I will execute before every method in Day2 class");
+    }
+
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("beforeClass.....I will execute before all methods in Day2 class");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("afterClass.....I will execute after all methods in Day2 class");
+    }
+
+
 }
