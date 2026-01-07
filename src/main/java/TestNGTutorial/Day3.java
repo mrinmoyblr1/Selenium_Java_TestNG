aeeb15dd3b6ce3dd9de52b8b58512fc14c9899aa
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-06 23:18:17 -0500

    BISWAS

--- src/main/java/TestNGTutorial/Day3.java
+++ src/main/java/TestNGTutorial/Day3.java
@@ -14,7 +14,10 @@ public class Day3 {
         System.out.println("Hello World from MobileLoginCarLoan");
     }
 
-    @Test
+    // In the below test, enabled is set to false, so this test will be skipped during execution
+    // It is usefull when we have a test that is not ready yet and we want to skip it for now
+    // We can enable it later by setting enabled to true or removing the attribute
+    @Test(enabled = false)
     public void MobileSignInCarLoan() {
         System.out.println("Hello World from MobileSignInCarLoan");
     }
@@ -26,7 +29,9 @@ public class Day3 {
     }
 
 
-    @Test(dependsOnMethods = {"WebLoginCarLoan", "MobileLoginCarLoan"})
+    // Here @Test is dependent on the successful execution of WebLoginCarLoan method
+    // If WebLoginCarLoan fails or is skipped, then APICarLoan will be skipped
+    @Test(dependsOnMethods = {"WebLoginCarLoan"})
     public void APICarLoan() {
         System.out.println("Hello World from LoginAPICarLoan");
     }
