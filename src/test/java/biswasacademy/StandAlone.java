8ab27db644b71fffc32de74c0e0b7f8e55dc551f
mrinmoyblr2 <mrinmoy.blr2@gmail.com>
2026-01-08 10:46:41 -0500

    BISWAS

---
+++ src/test/java/biswasacademy/StandAlone.java
@@ -0,0 +1,23 @@
+package biswasacademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StandAlone {
+    public static void main(String[] args) {
+
+        WebDriverManager.chroemeDriver().setup();
+
+
+
+
+
+//        System.out.println("This is a standalone Java class.");
+//        System.setProperty("webdriver.chrome.driver", "/Users/mrinmoy/IdeaProjects/Introduction/src/main/java/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+
+
+    }
+
+
+}
