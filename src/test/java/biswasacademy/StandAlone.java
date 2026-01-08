548b67f7fc30f22b333226ed0a938a04a74bec27
mrinmoyblr2 <mrinmoy.blr2@gmail.com>
2026-01-08 11:22:58 -0500

    BISWAS

--- src/test/java/biswasacademy/StandAlone.java
+++ src/test/java/biswasacademy/StandAlone.java
@@ -1,13 +1,14 @@
 package biswasacademy;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 public class StandAlone {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -17,9 +18,12 @@ public class StandAlone {
         System.out.println(driver.getTitle());
 
 
+        driver.findElement(By.id("userEmail")).sendKeys("mrinmoy.blr@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("Anjali@12");
+        driver.findElement(By.id("login")).click();
 
 
-
+        Thread.sleep(3000);
         driver.quit();
     }
 
