763d2b26fe54c545a068109332effd9b014ad0b4
mrinmoyblr2 <mrinmoy.blr2@gmail.com>
2026-01-08 14:22:51 -0500

    BISWAS

--- src/test/java/biswasacademy/StandAlone.java
+++ src/test/java/biswasacademy/StandAlone.java
@@ -3,9 +3,11 @@ package biswasacademy;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class StandAlone {
     public static void main(String[] args) throws InterruptedException {
@@ -22,8 +24,21 @@ public class StandAlone {
         driver.findElement(By.id("userPassword")).sendKeys("Anjali@12");
         driver.findElement(By.id("login")).click();
 
+        Thread.sleep(2000);
 
-        Thread.sleep(3000);
+
+        List<WebElement> products = driver.findElements(By.className("card-body"));
+
+        WebElement prod = products.stream().filter(product -> product.findElement(By.cssSelector("b"))
+                .getText().equalsIgnoreCase("ADIDAS ORIGINAL")).findFirst().orElse(null);
+
+        //prod.findElement(By.cssSelector(".card-body button:first-of-type")).click(); // Here :first-of-type is used to select the first button (Add to Wishlist)
+        prod.findElement(By.cssSelector(".card-body button:last-of-type")).click(); // Here :last-of-type is used to select the last button (Add to Cart)
+
+
+
+
+        Thread.sleep(6000);
         driver.quit();
     }
 
