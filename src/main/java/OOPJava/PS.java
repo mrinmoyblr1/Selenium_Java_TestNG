27a1554c3907cb118b77641517220b5a9b63d203
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-07 21:28:28 -0500

    BISWAS

---
+++ src/main/java/OOPJava/PS.java
@@ -0,0 +1,7 @@
+package OOPJava;
+
+public class PS {
+    public static void main(String[] args) {
+        System.out.println("PS");
+    }
+}
---
+++ src/main/java/OOPJava/PS1.java
@@ -0,0 +1,7 @@
+package OOPJava;
+
+public class PS1 extends PS {
+    public static void main(String[] args) {
+        System.out.println("PS1");
+    }
+}
