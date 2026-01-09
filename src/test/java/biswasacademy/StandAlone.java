fa6b14a308c30fd8ce5851623dda173677146a02
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-09 01:40:34 -0500

    BISWAS

--- src/test/java/biswasacademy/StandAlone.java
+++ src/test/java/biswasacademy/StandAlone.java
@@ -5,8 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.List;
@@ -16,6 +18,7 @@ public class StandAlone {
         String productName = "ZARA COAT 3";
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("https://rahulshettyacademy.com/client/#/auth/login");
         System.out.println(driver.getTitle());
@@ -28,8 +31,9 @@ public class StandAlone {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("card-body")));
         List<WebElement> products = driver.findElements(By.className("card-body"));
 
-        WebElement prod = products.stream().filter(product -> product.findElement(By.cssSelector("b"))
-                .getText().equalsIgnoreCase(productName)).findFirst().orElse(null);
+        WebElement prod = products.stream().
+                filter(product -> product.findElement(By.cssSelector("b"))
+                        .getText().equalsIgnoreCase(productName)).findFirst().orElse(null);
         //prod.findElement(By.cssSelector(".card-body button:first-of-type")).click(); // Here :first-of-type is used to select the first button (Add to Wishlist)
         prod.findElement(By.cssSelector(".card-body button:last-of-type")).click(); // Here :last-of-type is used to select the last button (Add to Cart)
 
@@ -40,17 +44,31 @@ public class StandAlone {
         System.out.println(driver.findElement(By.cssSelector("#toast-container")).getText());
         wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
 
-        //Thread.sleep(2000);
+        Thread.sleep(2000);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[routerlink*='cart']")));
         driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
 
 
-        List<WebElement> allProductsUnderCart = driver.findElements(By.cssSelector(".cartSection h3"));
-        System.out.println(allProductsUnderCart.size());
-        System.out.println(allProductsUnderCart.get(0).getText());
+        List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+        boolean match = cartProducts.stream().anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
+        System.out.println(match);
+        Assert.assertTrue(match);
 
-        System.out.println(allProductsUnderCart.stream().anyMatch(product -> product.getText().contains(productName)));
-        //allProductsUnderCart.stream().forEach(product -> product.getText().contains(productName));
+        driver.findElement(By.cssSelector("li[class='totalRow'] button[type='button']")).click();
+
+
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "India").build().perform();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+        driver.findElement(By.cssSelector(".ta-item:nth-of-type(2)")).click();
+        //    .ta-item:nth-of-type(2)
+        //    //button[contains(@class,'ta-item')][2]
+        driver.findElement(By.cssSelector(".action__submit")).click();
+
+        String confirmation = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        System.out.println(confirmation);
+        Assert.assertTrue(confirmation.equalsIgnoreCase("Thankyou for the order."));
+
 
         Thread.sleep(2000);
         driver.quit();
