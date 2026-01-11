67878d804b8db93462f8b5312e0a8a19ef50b110
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-11 11:10:43 -0500

    BISWAS

--- src/test/java/biswasacademy/TestComponents/BaseTest.java
+++ src/test/java/biswasacademy/TestComponents/BaseTest.java
@@ -1,18 +1,45 @@
 package biswasacademy.TestComponents;
 
+import biswasacademy.pageObjects.LandingPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseTest {
+    public WebDriver driver;
+    //Properties prop;
 
-    public void initializeDriver() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public WebDriver initializeDriver() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/biswasacademy/resources/GlobalData.properties");
+        prop.load(fis);
+        String browser = prop.getProperty("browser");
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            System.out.println("Edge is not supported yet");
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        return driver;
 
     }
+
+
+    public LandingPage launchApplication() throws IOException {
+        driver = initializeDriver();
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.goTo();
+        return landingPage;
+    }
+
 }
--- src/test/java/biswasacademy/Tests/SubmitOrderTest.java
+++ src/test/java/biswasacademy/Tests/SubmitOrderTest.java
@@ -1,43 +1,30 @@
 package biswasacademy.Tests;
 
+import biswasacademy.TestComponents.BaseTest;
 import biswasacademy.pageObjects.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import java.time.Duration;
+import java.io.IOException;
 
-public class SubmitOrderTest {
-    public static void main(String[] args) throws InterruptedException {
+public class SubmitOrderTest extends BaseTest {
+    @Test
+    public void submitOrder() throws IOException, InterruptedException {
         String productName = "ZARA COAT 3";
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
+        LandingPage landingPage = launchApplication();
 
-        LandingPage landingPage = new LandingPage(driver);
-        landingPage.goTo();
         ProductCatalogue productCatalogue = landingPage.loginApplication("mrinmoy.blr@gmail.com", "Anjali@12");
-
-
         productCatalogue.addProductToCart(productName);
 
-
         CartPage cartPage = productCatalogue.goToCartPage();
-
-
         boolean match = cartPage.verifyProductDisplaying(productName);
         Assert.assertTrue(match);
 
         CheckoutPage checkoutPage = cartPage.goToCheckOut();
-
-
         checkoutPage.selectCountry("India");
+
         ConfirmationPage confirmationPage = checkoutPage.submitOrder();
-
-
         String confirmation = confirmationPage.getConfirmationMessage();
         System.out.println(confirmation);
         Assert.assertTrue(confirmation.equalsIgnoreCase("Thankyou for the order."));
