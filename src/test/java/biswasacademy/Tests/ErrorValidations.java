cb4daeb07f0b36ecb283eddde83e472acd9d335c
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-11 15:42:56 -0500

    BISWAS

--- src/test/java/biswasacademy/Tests/ErrorValidations.java
+++ src/test/java/biswasacademy/Tests/ErrorValidations.java
@@ -1,15 +1,34 @@
 package biswasacademy.Tests;
 
 import biswasacademy.TestComponents.BaseTest;
+import biswasacademy.pageObjects.CartPage;
+import biswasacademy.pageObjects.ProductCatalogue;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class ErrorValidations extends BaseTest {
     @Test
-    public void submitOrder() {
+    public void loginErrorValidation() {
 
         landingPage.loginApplication("mrinmoy.blr@gmail.com", "Anjali@1212");
         Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
         System.out.println("Error message displayed" + ": " + landingPage.getErrorMessage());
     }
+
+
+    @Test
+    public void productErrorValidation() throws IOException, InterruptedException {
+        String productName = "ZARA COAT 3";
+
+        ProductCatalogue productCatalogue = landingPage.loginApplication("mrinmoy.blr@gmail.com", "Anjali@12");
+        productCatalogue.addProductToCart(productName);
+
+        CartPage cartPage = productCatalogue.goToCartPage();
+        boolean match = cartPage.verifyProductDisplaying("ZARA COAT 4");
+        Assert.assertFalse(match);
+    }
+
+
 }
