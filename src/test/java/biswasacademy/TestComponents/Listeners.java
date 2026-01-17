89fca7bbe91de6787ae35d37ebabf1d86fea55ed
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-17 18:55:55 -0500

    BISWAS

--- src/test/java/biswasacademy/TestComponents/Listeners.java
+++ src/test/java/biswasacademy/TestComponents/Listeners.java
@@ -16,10 +16,9 @@ public class Listeners implements ITestListener {
     WebDriver driver = null;
     BaseTest baseTest = new BaseTest();
 
+
     ExtentReports extent = ExtentReporterNG.getReportObject();
     ExtentTest test;
-
-
     ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
 
 
