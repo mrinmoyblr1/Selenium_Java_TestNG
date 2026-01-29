39a23ac3198077d8b30806c384d4501e9c52d2e9
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-01-29 01:35:24 -0500

    BISWAS

--- src/test/java/biswasacademy/stepDefinitions/StepDefinitionsImp.java
+++ src/test/java/biswasacademy/stepDefinitions/StepDefinitionsImp.java
@@ -1,7 +1,9 @@
 package biswasacademy.stepDefinitions;
 
+import io.cucumber.java.en.Given;
+
 public class StepDefinitionsImp {
-    @Given("I landed on E-commerce Page")
+    @Given("I landed on Ecommerce Page")
     public void I_landed_on_Ecommerce_Page() {
 
     }
