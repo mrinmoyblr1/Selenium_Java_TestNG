6cdd4987bb12084b64b3e95a0e69c7227a9de9c4
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-02-01 15:44:05 -0500

    BISWAS

--- src/test/java/biswasacademy/stepDefinitions/StepDefinitionsImp.java
+++ src/test/java/biswasacademy/stepDefinitions/StepDefinitionsImp.java
@@ -1,85 +1,53 @@
-//package biswasacademy.stepDefinitions;
-//
-//import biswasacademy.TestComponents.BaseTest;
-//import biswasacademy.pageObjects.ConfirmationPage;
-//import biswasacademy.pageObjects.LandingPage;
-//import biswasacademy.pageObjects.ProductCatalogue;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import io.cucumber.java.en.When;
-//import org.testng.Assert;
-//
-//import java.io.IOException;
-//
-//public class StepDefinitionsImp extends BaseTest {
-//    public LandingPage landingPage;
-//    ProductCatalogue productCatalogue;
-//    ConfirmationPage confirmationPage;
-//
-//
-//    @Given("I landed on Ecommerce Page")
-//    public void I_landed_on_Ecommerce_Page() throws IOException {
-//        landingPage = launchApplication();
-//
-//    }
-//
-//
-//    @Given("Logged in with username {string} and password {string}")
-//    public void Logged_in_with_username_and_password(String username, String password) {
-//        productCatalogue = landingPage.loginApplication(username, password);
-//    }
-//
-//
-//    @When("I add product {string} to Cart")
-//    public void I_add_product_to_Cart(String productName) throws InterruptedException {
-//        productCatalogue.addProductToCart(productName);
-//    }
-//
-//
-////    @And("Checkout {string} and Submit the order")
-////    public void checkout_and_submit_the_order(String string) throws InterruptedException {
-////        CartPage cartPage = productCatalogue.goToCartPage();
-////
-////        boolean match = cartPage.verifyProductDisplaying(string);
-////        Assert.assertTrue(match);
-////
-////        CheckoutPage checkoutPage = cartPage.goToCheckOut();
-////        checkoutPage.selectCountry("India");
-////
-////        confirmationPage = checkoutPage.submitOrder();
-////
-////    }
-//
-//
-//
-//
-//    @When("I add product productName to Cart")
-//    public void i_add_product_product_name_to_cart() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//
-//    @When("Checkout productName and Submit the order")
-//    public void checkout_product_name_and_submit_the_order() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//
-//
-//
-//
-//
-//
-//
-//    @Then("{string} message is displayed on Confirmation page")
-//    public void message_is_displayed_Confirmation_page(String message) {
-//        String confirmation = confirmationPage.getConfirmationMessage();
-//        System.out.println(confirmation);
-//        Assert.assertTrue(confirmation.equalsIgnoreCase(message));
-//
-//    }
-//
-//
-//}
-//
-//
+package biswasacademy.stepDefinitions;
+
+import biswasacademy.TestComponents.BaseTest;
+import biswasacademy.pageObjects.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+import java.io.IOException;
+
+public class StepDefinitionsImp extends BaseTest {
+    public LandingPage landingPage;
+    ProductCatalogue productCatalogue;
+    ConfirmationPage confirmationPage;
+
+
+    @Given("I landed on Ecommerce Page")
+    public void I_landed_on_Ecommerce_Page() throws IOException {
+        landingPage = launchApplication();
+
+    }
+
+    @Given("Logged in with username {string} and password {string}")
+    public void Logged_in_with_username_and_password(String username, String password) {
+        productCatalogue = landingPage.loginApplication(username, password);
+    }
+
+
+    @When("I add product {string} to Cart")
+    public void I_add_product_to_Cart(String productName) throws InterruptedException {
+        productCatalogue.addProductToCart(productName);
+    }
+
+
+    @When("Checkout {string} and Submit the order")
+    public void Checkout_productName_and_Submit_the_order(String string) throws InterruptedException {
+
+
+        CartPage cartPage = productCatalogue.goToCartPage();
+
+        boolean match = cartPage.verifyProductDisplaying(string);
+        Assert.assertTrue(match);
+
+        CheckoutPage checkoutPage = cartPage.goToCheckOut();
+        checkoutPage.selectCountry("India");
+
+        confirmationPage = checkoutPage.submitOrder();
+    }
+
+
+}
+
+
--- src/test/java/cucumber/SubmitOrderBack.feature
+++ src/test/java/cucumber/SubmitOrderBack.feature
@@ -7,8 +7,8 @@ Feature: Purchase Order from E-commerce Website
 
   Scenario Outline: Positive Test of Submitting the order
     Given Logged in with username "<name>" and password "<password>"
-    When I add product  to Cart
-    And Checkout  and Submit the order
+    When I add product "<productName>" to Cart
+    And Checkout "<productName>" and Submit the order
 
     Examples:
       | name                  | password  | productName |
