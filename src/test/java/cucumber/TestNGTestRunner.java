c1d642394b23b5c559110c6d15b405a8b895c6d6
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-02-02 15:17:25 -0500

    BISWAS

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -3,7 +3,8 @@ package cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/cucumber", glue = "biswasacademy.stepDefinitions", plugin = {"html:target/cucumber.html"}, monochrome = true,tags = "@ErrorValidation")
+@CucumberOptions(features = "src/test/java/cucumber", glue = "biswasacademy.stepDefinitions", plugin = {"html:target/cucumber.html"},
+        monochrome = true, tags = "@ErrorValidation")
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
 }
